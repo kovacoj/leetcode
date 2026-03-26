b318e336c03eb34eedf84f70e09b6acc3aab0e82
Ján Kovačovský <kovacoj1@gmail.com>
2026-03-26 16:22:22 +0100

    problem(104): add dfs solution

---
+++ problems/104.maximum-depth-of-binary-tree.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=104 lang=python3
+#
+# [104] Maximum Depth of Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        return 0 if not root else max((
+            self.maxDepth(node) for node in (root.left, root.right) if node
+        ), default=0) + 1
+        
+# @lc code=end
+
